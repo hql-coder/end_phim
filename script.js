81f061e62582fbba069015ec889e084c485112d8
hql-coder <taikhoancoder@gmail.com>
2026-09-24 17:39:29 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,19 +7,14 @@ document.addEventListener('DOMContentLoaded', () => {
         const star = document.createElement('div');
         star.classList.add('star');
 
-        // Kích thước ngẫu nhiên
         const size = Math.random() * 3 + 1;
         star.style.width = size + 'px';
         star.style.height = size + 'px';
 
-        // Vị trí ngẫu nhiên
         star.style.left = Math.random() * 100 + '%';
         star.style.top = Math.random() * 100 + '%';
 
-        // Độ sáng ngẫu nhiên
         star.style.opacity = Math.random() * 0.7 + 0.3;
-
-        // Thời gian nhấp nháy ngẫu nhiên
         star.style.animationDuration = (Math.random() * 3 + 2) + 's';
         star.style.animationDelay = (Math.random() * 3) + 's';
 
